2310551be95f4fca59515dcfb913871e977c1d57
s3949273 <s3949273@student.rmit.edu.au>
2025-03-31 18:49:50 +1100

    added support for windows

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use chrono::Utc;
 use exif::{Reader, Tag, In};
 use std::env;
 
+const VALID_FILE_EXTENSIONS:[&str;2] = ["png", "jpg"];
+const OS:&str = env::consts::OS;  
+
 fn calc_date(dur:std::time::Duration)->String{
     let mut today = Utc::now();
     today = today.checked_sub_signed(chrono::Duration::seconds(dur.as_secs() as i64)).unwrap();
@@ -66,23 +69,28 @@ fn append_to_vec(hm:& mut HashMap<String, Vec<String>>,key:String, path:String){
     }
 }
 
-fn do_orig_files(path:String)->HashMap<String, Vec<String>>{
+fn resolve_paths(path:String)->HashMap<String, Vec<String>>{
     //only run when given a folder path
     let mut ret:HashMap<String, Vec<String>> = HashMap::new();
     let md = metadata(String::from(&path)).unwrap();
     if md.is_dir(){
         //sort files here
         let dir = read_dir(String::from(&path)).unwrap();
+        
         for dir_res in dir{
-            let dir_res = dir_res.unwrap();
+            let dir_res= dir_res.unwrap();
+            let dir_path = dir_res.path();
+            let path = dir_path.to_str().expect(&format!("cannot convert path of {:?} ",dir_path.to_string_lossy()));
             let md = dir_res.metadata().unwrap();
+            // println!("{:?}",&path.get(path.rfind(".").unwrap()+1..).unwrap());
             if md.is_dir(){
                 //recurse 
-                ret.extend(do_orig_files(String::from(dir_res.path().to_str().unwrap())));
-            }else{
-                let path = String::from(dir_res.path().to_str().unwrap());
-                let file_date = get_file_date(path.clone());
-                append_to_vec(&mut ret, file_date, path.clone());
+                ret.extend(resolve_paths(String::from(dir_path.to_str().unwrap())));
+
+            }else if VALID_FILE_EXTENSIONS.contains(&path.get(path.rfind(".").unwrap()+1..).unwrap_or_default()){
+
+                let file_date = get_file_date(path.to_string().clone());
+                append_to_vec(&mut ret, file_date, path.to_string().clone());
             }
         }   
     }else{
@@ -96,11 +104,12 @@ fn do_orig_files(path:String)->HashMap<String, Vec<String>>{
 fn get_file_date(path:String)->String{
     //returns empty string if not good
     let mut ret:String = String::new();
-    match File::open(String::from(path)){
+    match File::open(String::from(path.clone())){
         Ok(file)=>{
             match Reader::new().read_from_container(&mut BufReader::new(&file)){
                 Ok(exif)=>{
-                    match exif.get_field(Tag::DateTimeDigitized, In::PRIMARY){
+                    println!("exif data found for: {:?} ", path.clone());
+                    match exif.get_field(Tag::DateTimeOriginal, In::PRIMARY){
                         Some(date)=>{
                             // println!("EXIFdate: {:?}",date.display_value().to_string());
                             ret = date.display_value().to_string();
@@ -112,6 +121,7 @@ fn get_file_date(path:String)->String{
                         }
                     }
                 }Err(_)=>{
+                    println!("No Exif Data found for file: {:?}", path);
                     ret = calc_date(file.metadata().unwrap().created().unwrap().elapsed().unwrap());
                     ret = ret.replace(":", "-").replace(" ", "-");
                 }
@@ -124,23 +134,26 @@ fn get_file_date(path:String)->String{
     ret
 }
 
-fn resolve_dest_path(path:String, dest_file_path:String)->String{
+fn resolve_dest_path(path:String, dest_file_path:String, directory_separator:&str)->String{
     let mut ret = String::from(dest_file_path.clone());
-    ret+="/";
+    println!("{path:?}");
+    ret+=directory_separator;
     ret += &path[0..4];
-    ret +="/";
+    ret +=directory_separator;
     ret += &path[5..7];
-    ret +="/";
+    ret +=directory_separator;
     ret += &path;
     println!("{ret:?}.jpg");
     ret
 }
 
-fn copy_files(path_map: &HashMap<String, Vec<String>>, dest_file_path:String){
+fn copy_files(path_map: &HashMap<String, Vec<String>>, dest_file_path:String, directory_separator:&str){
     for (k,v) in path_map{
-        println!("v[0]={:?}; destination path resolved as: {:?}",v[0].clone(),resolve_dest_path(k.clone(), dest_file_path.clone())+".jpg");
+        let file_extension = v[0].get(v[0].rfind(".").unwrap()..).unwrap();
+        println!("file extension: {file_extension:?}");
+        println!("v[0]={:?}; destination path resolved as: {:?}",v[0].clone(),resolve_dest_path(k.clone(), dest_file_path.clone(), directory_separator)+v[0].get(v[0].rfind(".").unwrap()..).unwrap());
         //only do the first file
-        match copy(v[0].clone(),resolve_dest_path(k.clone(), dest_file_path.clone())+".jpg"){
+        match copy(v[0].clone(),resolve_dest_path(k.clone(), dest_file_path.clone(),directory_separator)+v[0].get(v[0].rfind(".").unwrap()..).unwrap()){
             Ok(_)=>{
 
             }Err(e)=>{
@@ -150,7 +163,7 @@ fn copy_files(path_map: &HashMap<String, Vec<String>>, dest_file_path:String){
         if v.len() >1 {
             for counter in 1..v.len(){
                 //copy the files over given the counter i.e. path_to_dest_folder/yyyy/mm/yyyy-mm-dd-hh-mm-ss-c.jpg
-                match  copy(v[0].clone(), resolve_dest_path(k.clone(), dest_file_path.clone())+"-"+&counter.to_string()+".jpg"){
+                match  copy(v[0].clone(), resolve_dest_path(k.clone(), dest_file_path.clone(), directory_separator)+"-"+&counter.to_string()+v[0].get(v[0].rfind(".").unwrap()..).unwrap()){
                     Ok(_)=>{
 
                     }Err(e)=>{
@@ -208,18 +221,33 @@ fn take_recurse_input()->bool{
 fn print_help(){
     println!("the program expects 2 command line arguments, they are:");
     println!("[path_to_files] and the keyword \"recurse\" which will allow the program to recurse into sub folders given a directory");
-    println!("An example is: cargo run -- /test/test1 recurse");
+    println!("An example is: cargo run /test/test1 recurse");
     println!("Another example is: cargo run -- /test/test1");
 
 }
 
 
 fn main() { 
+    // grab os info
+      
+    // linux / unix based systems
+    let mut directory_separator:&str = "/";
+    if OS == "windows"{
+        directory_separator = "\\";
+    }
+
+
     println!("Using the kamadak-exif rust crate: https://crates.io/crates/kamadak-exif");
     println!("\ntime will be in UTC time not AEST\n");
+    println!("\n\n");
+    println!("detected OS: {OS:?}");
+    println!("\n\n");
+    // print_help();
     let args:Vec<String> = env::args().collect();
+    println!("args: {args:?}");
     if args.len() == 1 {
-        println!("please enter the source and destination file paths in the command line");
+        println!("### WARNING ###\n");
+        print_help();
     }else{
         let orig_file_path:String = args[1].clone();
         println!("got: {}",orig_file_path.clone());
@@ -228,14 +256,12 @@ fn main() {
 
         println!("starting...");
 
-        let path_map = do_orig_files(orig_file_path.clone());
+        let path_map = resolve_paths(orig_file_path.clone());
         println!("finished getting new filepaths...\nstarting to create directories needed");
         create_dir_structure(&path_map, dest_file_path.clone());
         println!("finished creating the directories needed... \nstarting the copying process...");
-        copy_files(&path_map, dest_file_path.clone());
+        copy_files(&path_map, dest_file_path.clone(), directory_separator);
 
-        
-        
         println!("finished");
     }
 }
